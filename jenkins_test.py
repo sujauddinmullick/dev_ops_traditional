f81f4f7358339a1a45535ee7c2850385945b5c1d
user33 <sujauddinmullick@yahoo.in>
2016-02-04 12:20:18 +0530

    ldap jenkins script

--- jenkins_test.py
+++ jenkins_test.py
@@ -8,12 +8,19 @@ import os
 import subprocess
 from scp import SCPClient
 import time
+import sys
+
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('devOps_install')
 
-host1 = '172.30.36.216'
-host2 = '172.30.36.156'
-host3 = '172.30.36.119'
+if (len(sys.argv) < 3):
+    print "invalid usage. Required args <jenkins_host> <jenkins_url>."
+    exit(1)
+
+
+host = sys.argv[1]
+jenkins_url=sys.argv[2]
+
 
 logger.info('Started')
 
@@ -30,82 +37,94 @@ def remote_connect(host,user,passwd):
         logger.critical('Failed to connect %s : %s' % (host,se))
         exit(1)
 
-def install_jenkins(host,user,passwd):
-    client2 = host
+def install_jenkins(client1):
+    client = client1
     #client2 = remote_connect(host1,user,passwd)
     try:
-        stdin, stdout, stderr = client2.exec_command('wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo')
+        stdin, stdout, stderr = client.exec_command('wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo')
         if stdout:
             res = stdout.readlines()
             result= ''.join(res)
-            logger.info('install_jenkins wget on %s :%s' %(host1,result))
-        stdin, stdout, stderr = client2.exec_command('rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key')
+            logger.info('install_jenkins wget on %s :%s' %(host,result))
+        stdin, stdout, stderr = client.exec_command('rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key')
         if stdout:
             res = stdout.readlines()
             result= ''.join(res)
-            logger.info('install_jenkins rpm --import on %s :%s' %(host1,result))
-        stdin, stdout, stderr = client2.exec_command('yum install -y jenkins')
+            logger.info('install_jenkins rpm --import on %s :%s' %(host,result))
+        stdin, stdout, stderr = client.exec_command('yum install -y jenkins')
         if stdout:
             res = stdout.readlines()
             result= ''.join(res)
-            logger.info('install_jenkins yum install on %s :%s' %(host1,result))
+            logger.info('install_jenkins yum install on %s :%s' %(host,result))
     except SSHException,e:
         logger.critical('install_jenkins exceptions %s' %(str(e)))
 
-def restart_jenkins(host,user,passwd):
-    client2 = host
+def restart_jenkins(client2):
+    client = client2
     #client2 = remote_connect(host1,user,passwd)
     try:
-        stdin, stdout, stderr = client2.exec_command('sed -i \'s/JENKINS_PORT=\"8080\"/JENKINS_PORT=\"8082\"/g\' /etc/sysconfig/jenkins')
+        stdin, stdout, stderr = client.exec_command('sed -i \'s/JENKINS_PORT=\"8080\"/JENKINS_PORT=\"8082\"/g\' /etc/sysconfig/jenkins')
         if stderr:
             res = stderr.readlines()
             result= ''.join(res)
-            logger.critical('restart_jenkins http_port chnage on %s :%s' %(host1,result))
-        stdin, stdout, stderr = client2.exec_command('service jenkins restart ')
+            logger.critical('restart_jenkins http_port chnage on %s :%s' %(host,result))
+        stdin, stdout, stderr = client.exec_command('service jenkins restart ')
         if stderr:
             res = stderr.readlines()
             result= ''.join(res)
             res1 = stdout.readlines()
             result1 = ''.join(res1)
             logger.info('restart_jenkins restart out: %s err:%s' %(result1,result))
-        stdin, stdout, stderr = client2.exec_command('chkconfig jenkins on')
+        stdin, stdout, stderr = client.exec_command('chkconfig jenkins on')
         if stderr:
             res = stderr.readlines()
             result= ''.join(res)
-            logger.critical('restart_jenkins chkconfig on %s :%s' %(host1,result))
-        stdin, stdout, stderr = client2.exec_command('systemctl stop firewalld.service')
+            logger.critical('restart_jenkins chkconfig on %s :%s' %(host,result))
+        stdin, stdout, stderr = client.exec_command('systemctl stop firewalld.service')
         if stdout:
             res = stdout.readlines()
             result= ''.join(res)
-            logger.info('restart_jenkins firewalld %s :%s' %(host1,result))
+            logger.info('restart_jenkins firewalld %s :%s' %(host,result))
     #client2.close()
     except SSHException,e:
         logger.critical('restart_jenkins exceptions %s' %(str(e)))
 
 
-client1 = remote_connect(host2,'root','p@ssw0rd')
+client1 = remote_connect(host,'root','p@ssw0rd')
 #do a clean up before installing
 
-#install_jenkins(client1,'root','p@ssw0rd')
+install_jenkins(client1)
 
-#time.sleep(300)
+time.sleep(60)
 
-#restart_jenkins(client1,'root','p@ssw0rd')
+restart_jenkins(client1)
 
-#jenkins_url = 'http://'+host2+'8082'
+time.sleep(120)
 
-#install required plug-ins
-'''
-#success
-prog = subprocess.Popen('java -jar jenkins-cli.jar -s http://172.30.36.156:8082 install-plugin jira jenkins-jira-issue-updater gerrit-trigger git-client git -deploy -restart', stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+#copy few required configuration files to temp location
+
+scp = SCPClient(client1.get_transport())
+scp.put('devops_config/jenkins/plugins.txt','/usr/local/etc')
+scp.put('devops_config/jenkins/gerrit-trigger.xml','/usr/local/etc')
+scp.put('devops_config/jenkins/jenkins.model.JenkinsLocationConfiguration.xml','/usr/local/etc')
+scp.put('devops_config/jenkins/jenkins-cli.jar','/usr/local/etc')
+
+#copy the gerrit configuration xml file
+scp.put('devops_config/jenkins/gerrit-trigger.xml','/var/lib/jenkins/')
+#copy the jenkins own location xml file
+scp.put('devops_config/jenkins/jenkins.model.JenkinsLocationConfiguration.xml','/var/lib/jenkins/')
+#copy the authentication method xml file
+scp.put('devops_config/jenkins/config.xml','/var/lib/jenkins/')
+
+#install required plugins
+
+prog = subprocess.Popen('java -jar jenkins-cli.jar -s '+jenkins_url+' install-plugin jira jenkins-jira-issue-updater gerrit-trigger git-client git -deploy -restart', stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
 out,err = prog.communicate()
 if err:
     logger.critical('jenkins plugin install error %d %s' %(prog.returncode,err.strip()))
-#creating gerrit server connections in jenkins
 
-'''
 #jenkins job creation
-prog = subprocess.Popen('java -jar jenkins-cli.jar -s http://172.30.36.156:8082 create-job job100 < jenkins_job.xml', stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+prog = subprocess.Popen('java -jar jenkins-cli.jar -s '+jenkins_url+' create-job job100 < devops_config/jenkins/jenkins_job_ldap.xml', stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
 out,err = prog.communicate()
 if err:
     logger.critical('jenkins create_job error %d %s' %(prog.returncode,err.strip()))
